60954269e4d299f39a35a793fdda2375773a9817
Maykon Granemann <maykondgranemann@gmail.com>
2025-09-03 20:36:24 -0300

    feat(person): created new person structure

--- src/pages/person/person-list/person-list.js
+++ src/pages/person/person-list/person-list.js
@@ -1,12 +1,12 @@
 let table = document.querySelector('table');
 if(table){
     let tbody = table.querySelector('tbody');
-    list = localStorage.getItem("personList")
+    let list = localStorage.getItem("personList")
     if(list){
         tbody.innerHTML = ''; 
-        listPeople = JSON.parse(list);
+        let listPeople = JSON.parse(list);
         listPeople.forEach(function(person) {
-            var row = document.createElement('tr');
+            let row = document.createElement('tr');
             row.innerHTML = '<td>' + person.id + '</td>' +
                             '<td>' + person.firstName + '</td>' +
                             '<td>' + person.lastName + '</td>' +
